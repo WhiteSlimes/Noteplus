91e51f15c4e8a4956579c5249d6aaeb8761d815d
WhiteSlimes <43409869+WhiteSlimes@users.noreply.github.com>
2021-09-17 14:53:46 +0200

    --> Versioning 1.2 <--  - Add RecycleView  - Start ColorNote

--- app/src/main/java/fr/papon/note/activities/MainActivity.java
+++ app/src/main/java/fr/papon/note/activities/MainActivity.java
@@ -8,17 +8,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.papon.note.R;
+import fr.papon.note.adapters.NotesAdapter;
 import fr.papon.note.database.NotesDatabase;
 import fr.papon.note.entities.Note;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView noteRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE);
             }
         });
+
+        noteRecyclerView = findViewById(R.id.notesRecyclerView);
+        noteRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        noteRecyclerView.setAdapter(notesAdapter);
 
         getNotes();
     }
@@ -47,10 +65,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if (noteList.size() == 0){
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                noteRecyclerView.smoothScrollToPosition(0);
             }
         }
 
         new GetNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK){
+            getNotes();
+        }
     }
 }
